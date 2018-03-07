d2202fc9c7b6021e75bc266243b7a7fffd3e82a0
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-03-07 14:05:47 +0100

    update index

--- index.php
+++ index.php
@@ -4,3 +4,13 @@ if(!isset($_COOKIE["user"])){
     header('location:disclaimer.php');
 }
 echo "HEJ";
+
+
+
+echo "<form method='POST'>
+<h2>Fyll i för att se ditt stjärntecken</h2>
+<input type='text' placeholder='Förnamn' name='firstname'/>
+<input type='text' placeholder='Efternamn' name='lastname'/>
+<input type='number' placeholder='YYMMDD' name='number'/>
+<input type='submit' value='send'/>
+</form>";
