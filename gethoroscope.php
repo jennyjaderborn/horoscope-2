0f32cafc46fef8b398bbdca712348da5cab57ed4
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-03-08 11:57:11 +0100

    update

--- gethoroscope.php
+++ gethoroscope.php
@@ -1,4 +1,6 @@
 <?php 
+include 'header.php';
+include 'footer.php';
 if(!isset($_COOKIE["user"])){
     echo"cookie is set<br/>";
     header('location:disclaimer.php');
--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php 
+
+
+include 'header.php';
+include 'footer.php';
 if(!isset($_COOKIE["user"])){
     echo"cookie is set<br/>";
     header('location:disclaimer.php');
 }
 echo "HEJ";
 
+?>
 
-
-echo "<form method='POST'>
 <h2>Fyll i för att se ditt stjärntecken</h2>
-<input type='text' placeholder='Förnamn' name='firstname'/>
-<input type='text' placeholder='Efternamn' name='lastname'/>
 <input type='number' placeholder='YYMMDD' name='number'/>
-<input type='submit' value='send'/>
-</form>";
+<button onclick="dateOfBirth()">skicka</button>
